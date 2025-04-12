f3eb6c8952f98dcf28160630e012cd9c6cd875e3
Bumpin That <guninmik@gmail.com>
2025-04-12 16:51:47 +0300

    Rewrite addresses crud views to CRUDRouter

--- core/api/addresses/views.py
+++ core/api/addresses/views.py
@@ -1,53 +1,23 @@
-import typing
-
-from fastapi import APIRouter, Depends, status
-from fastapi.responses import JSONResponse, Response
-from sqlalchemy.ext.asyncio import AsyncSession
-
 from core.api.addresses.schemas import AddressCreateSchema, AddressSchema, AddressUpdateSchema
 from core.api.addresses.services import AddressesCRUDService
+from core.api.routers import CRUDRouter, CRUDRouterConfig
 from core.api.users.dependencies import check_user_role
-from core.database import get_session
-from core.database.models.user import User, UserRole
+from core.database.models.user import UserRole
 
-router = APIRouter(prefix="/addresses", tags=["Addresses API"])
+config = CRUDRouterConfig(
+    "/addresses",
+    ["Addresses API"],
+    AddressCreateSchema,
+    AddressUpdateSchema,
+    AddressSchema,
+    AddressesCRUDService(),
+    {
+        "list": check_user_role(UserRole.CUSTOMER),
+        "create": check_user_role(UserRole.CUSTOMER),
+        "update": check_user_role(UserRole.CUSTOMER),
+        "delete": check_user_role(UserRole.CUSTOMER),
+    },
+)
 
-
-@router.get("/", response_model=list[AddressSchema])
-async def get_addresses(
-    session: typing.Annotated[AsyncSession, Depends(get_session)],
-    user: typing.Annotated[User, Depends(check_user_role(UserRole.CUSTOMER, exclude_admin=True))],
-) -> JSONResponse:
-    addresses_list = await AddressesCRUDService().get_entities_list(session, user)
-    return JSONResponse(addresses_list)
-
-
-@router.post("/", response_model=AddressSchema)
-async def create_address(
-    create_address_schema: AddressCreateSchema,
-    session: typing.Annotated[AsyncSession, Depends(get_session)],
-    user: typing.Annotated[User, Depends(check_user_role(UserRole.CUSTOMER, exclude_admin=True))],
-) -> JSONResponse:
-    address = await AddressesCRUDService().create_entity(create_address_schema, session, user)
-    return JSONResponse(address, status_code=status.HTTP_201_CREATED)
-
-
-@router.patch("/{address_id}", response_model=AddressSchema)
-async def update_address(
-    address_id: int,
-    update_address_schema: AddressUpdateSchema,
-    session: typing.Annotated[AsyncSession, Depends(get_session)],
-    user: typing.Annotated[User, Depends(check_user_role(UserRole.CUSTOMER))],
-) -> JSONResponse:
-    address = await AddressesCRUDService().update_entity(address_id, update_address_schema, session, user)
-    return JSONResponse(address, status_code=status.HTTP_200_OK)
-
-
-@router.delete("/{address_id}")
-async def delete_address(
-    address_id: int,
-    session: typing.Annotated[AsyncSession, Depends(get_session)],
-    user: typing.Annotated[User, Depends(check_user_role(UserRole.CUSTOMER))],
-) -> Response:
-    await AddressesCRUDService().remove_entity(address_id, session, user)
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
+crud_router = CRUDRouter(config)
+router = crud_router.router
