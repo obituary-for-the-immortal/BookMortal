8fc2e87542d4f53b765bd33b2b7228d25c2cef0a
Bumpin That <guninmik@gmail.com>
2025-04-15 18:20:59 +0300

    Add pydantic ValidationError except in error middleware

--- core/middleware/error.py
+++ core/middleware/error.py
@@ -1,11 +1,17 @@
 from fastapi import status
 from fastapi.requests import Request
 from fastapi.responses import JSONResponse
+from pydantic import ValidationError
 
 
 async def server_error_middleware(request: Request, call_next):
     try:
         return await call_next(request)
+    except ValidationError as e:
+        return JSONResponse(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            content={"message": "Validation error", "details": e.errors()},
+        )
     except Exception:  # noqa
         return JSONResponse(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
